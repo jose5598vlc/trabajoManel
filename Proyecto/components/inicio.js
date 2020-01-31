4d05768a80f9e44a99560ddd005762e95ce48709
ORDENADOR-XAVI\Xavi <xavcanfab@gmail.com>
2020-01-31 13:38:32 +0100

    Modifico inicio.js poniendo nombre de usuario (falta navigation a modificar)

--- Proyecto/components/inicio.js
+++ Proyecto/components/inicio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-
+    StyleSheet,
     Text,
     View,
     Button,
@@ -31,7 +31,7 @@ export default class Inicio extends Component {
 render(){
   return(
     <View style = {{backgroundColor: "blue"}}>
-      <Text> "Pantalla Inicio </Text>
+<Text style={styles.header}>Bienvenido{this.props.navigation.getParam('i')}</Text>
 
       <FlatList
         data={this.state.elementos}
@@ -59,11 +59,7 @@ render(){
             <Text>{item.descripcion}</Text>
           </View>
           <Text>{item.descripcion}</Text>
-          <Button title={"Modificar"} onPress={() =>{Alert.alert("Modifico: "+item.id);
-          fetch('http://localhost/3000/elementos/' + item.id, { method: 'PUT',})
-          .then(res => res.text())
-          .then(res => console.log(res))
-          }}
+          <Button title={"Modificar"} onPress={() => this.props.navigation.navigate("ModificarElemento", {i:item})}
           />
         </View>
           
@@ -81,128 +77,17 @@ render(){
 );
   };
 }
+  
 
-
-
-/*
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  ListView,
-  TouchableHighlight,
-  View
-} from 'react-native';
-export default class App extends Component<{}> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '',
-      lista: [],
-      dataSource: '',
-    };
-  }
-añadirItem() {
-    if (this.state.text != '') {
-      this.state.lista[this.state.lista.length] = this.state.text
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.setState({dataSource: ds.cloneWithRows(this.state.lista)});
-      this.setState({text: ''});
+  const styles = StyleSheet.create({
+    header: {
+        fontSize: 24,
+        color: '#fff',
+        paddingBottom: 10,
+        marginBottom: 40,
+        borderBottomColor: '#199187',
     }
-  }
-eliminarItem(item) {
-    for(var i = 0; i<this.state.lista.length; i++) {
-        if (this.state.lista[i] == item) {
-           delete this.state.lista[i];
-           break;
-        }
-    }
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.setState({dataSource: ds.cloneWithRows(this.state.lista)});
-  }
-render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => this.añadirItem()}>
-          <Text style={styles.textButton}>Añadir</Text>
-        </TouchableHighlight>
-      </View>
-      {this.state.dataSource != '' && <ListView
-        dataSource={this.state.dataSource}
-        renderRow={(rowData) =>
-          <View style={styles.listItem}>
-            <TouchableHighlight
-              style={styles.buttonItem}
-              onPress={() => this.eliminarItem(rowData)}>
-              <Text style={styles.textButton}>Eliminar</Text>
-            </TouchableHighlight>
-            <Text style = {styles.textItem}>{rowData}</Text>
-          </View>}
-      />}
-    </View>
-    );
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  header: {
-    flexDirection: 'row',
-  },
-  input: {
-    marginLeft: 10,
-    marginTop: 10,
-    width:250,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  button: {
-    alignItems: 'center',
-    width: 70,
-    height: 40,
-    backgroundColor: '#79D0FF',
-    borderRadius: 2,
-    marginLeft: 10,
-    marginTop: 10
-  },
-  textButton:{
-    marginTop: 10,
-  },
-  listItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: 350,
-    height: 70,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  buttonItem: {
-    alignItems: 'center',
-    width: 70,
-    height: 40,
-    backgroundColor: '#FF7979',
-    borderRadius: 2,
-    marginLeft: 10,
-  },
-  textItem: {
-    marginLeft: 30,
-  }
-}); 
-*/
-
-
+  )
 
 
